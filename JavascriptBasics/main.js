3cc158451f617553a0cbbfc5528d282c7262731f
SureshDeora <ss9672805637@gmail.com>
2024-09-16 20:49:24 +0530

    test

---
+++ JavascriptBasics/main.js
@@ -0,0 +1,10 @@
+const user1 = {firstName: "Suresh", gender: "male"};
+console.log(user1["gender"]);
+
+console.log(sum())
+function sum() {
+//  console.log(a+b);
+const num = 10;
+const name = "suresh";
+}
+// const value = sum(6, 4)
